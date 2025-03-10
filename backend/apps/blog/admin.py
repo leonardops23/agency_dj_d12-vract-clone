e739414aaf312919ee91253b9898c3f8a109e296
leonardo <ivanpachecoleo23@gmail.com>
2025-03-09 22:58:37 -0500

    add new models

--- backend/apps/blog/admin.py
+++ backend/apps/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import BlogPost, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(BlogPost)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'published_date', 'status')
+    list_filter = ('status', 'category', 'author', 'published_date')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published_date'
--- backend/apps/blog/models.py
+++ backend/apps/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self) -> str:
+        return self.name        
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.CharField(max_length=200)
+    featured_image = models.ImageField(upload_to='posts', blank=True, null=True)
+    excerpt = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    published_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10,
+        choices=[('draft', 'Draft'), ('published', 'Published')],
+        default='draft',
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published_date']
